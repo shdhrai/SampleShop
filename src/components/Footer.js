d98166ecdc1b56c95e46bb0bf8b227f5710ea29a
shdhrai <shdhrai@gmail.com>
2022-07-01 22:45:28 +0530

    Created Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="justify-content-center d-flex">
+        <div className="card-name">
+          <img
+            alt ="mastercard"
+            src ="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Mastercard_2019_logo.svg/1280px-Mastercard_2019_logo.svg.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt ="visa"
+            src ="https://1000logos.net/wp-content/uploads/2021/11/VISA-logo.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt ="paypal"
+            src ="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/1920px-PayPal.svg.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt ="express"
+            src ="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/American_Express_logo_%282018%29.svg/800px-American_Express_logo_%282018%29.svg.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt ="discover"
+            src ="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Discover_Card_logo.svg/1920px-Discover_Card_logo.svg.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
