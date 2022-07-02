0958b6db1c77db0a2a0de84c0f0e14660445674a
Anujjadaun <anujjadaun97@gmail.com>
2022-07-02 19:10:02 +0530

    added responsive.css index.html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './responsive.css';
 import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import CartScreen from './screens/CartScreen';
