c8d9188217894500e20cf27d7347b28d2ffd2e91
shdhrai <shdhrai@gmail.com>
2022-07-02 19:21:03 +0530

    Error Removed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ShopSection from '../components/homeComponents/ShopSection';
 import CalltoActionSection from '../components/homeComponents/CalltoActionSection';
-import ContactInfo from '../components/homeComponents/Contactinfo';
+import ContactInfo from '../components/homeComponents/ContactInfo';
 
 export default function HomeScreen() {
   return (
