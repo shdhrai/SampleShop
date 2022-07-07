82a3f314061923b6ccc720bb483c9e2dfe06d387
Anujjadaun <anujjadaun97@gmail.com>
2022-07-07 08:56:00 +0530

    added redux in shopsection and singleproduct

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import { productListReducer } from "./Reducers/ProductReducers";
+import { productDetailsReducer } from "./Reducers/ProductReducers";
 
 const reducer = combineReducers({
     productList : productListReducer,
+    productDetails:productDetailsReducer,
 });
 
 const initialState = {};
--- frontend/src/components/homeComponents/ShopSection.js
+++ frontend/src/components/homeComponents/ShopSection.js
@@ -1,58 +1,85 @@
-import React,{useState,useEffect} from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import CategorySection from "./CategorySection";
+import Loading from "./../LoadingError/Loading";
+import Message from "./../LoadingError/Error";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
-import Pagination from "./Pagination";
-import axios from 'axios';
+import { listProduct } from "./../../Redux/Actions/ProductActions";
+import Pagination from "./Pagination.js";
 
 const ShopSection = () => {
-    const [products,setProducts]=useState([]);
+  // const { keyword, pageNumber } = props;
 
-    useEffect(()=>{
-        const fetchproducts=async()=>{
-            const {data}=await axios.get("/api/products");
-            setProducts(data);
-        }
-        fetchproducts();
-    },[]);
-    return (
-        <>
-        <div className="container">
-            <div className="section">
-                <div className="row">
-                    <div className="col-lg-12 col-md-12 article">
-                        <div className="shopcontainer row">
-                            {products.map((product) =>(
-                                <div className="shop col-lg-4 col-md-6 col-sm-6 my-3" key={product._id}>
-                                    <div className="border-product">
-                                        <Link to={`/products/${product._id}`}>
-                                            <div className="shopBack">
-                                                <img src={product.image} alt={product.name}/>
-                                            </div>
-                                        </Link>
-                                        <div className="shoptext">
-                                            <p>
-                                                <Link to={`/products/${product._id}`}>
-                                                    {product.name}
-                                                </Link>
-                                            </p>
+  const dispatch = useDispatch();
 
-                                            <Rating
-                                               value={product.rating}
-                                               text ={`${product.numReviews}reviews`}
-                                               />
-                                               <h3>₹{product.price}</h3>
-                                        </div>
-                                    </div>
-                                </div>       
-                            ))}
-                            <Pagination/>
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProduct());
+  }, [dispatch]);
+  return (
+    <>
+      <div className="container">
+        <div className="section">
+          <div className="row">
+            {/* <div className="sidebar col-lg-3 col-md-3">
+              <CategorySection />
+            </div> */}
+
+            <div className="col-lg-12 col-md-12 article">
+              <div className="shopcontainer row">
+                {loading ? (
+                  <Loading />
+                ) : error ? (
+                  <Message variant="alert-danger">{error}</Message>
+                ) : (
+                  <>
+                    {products.map((product) => (
+                      <div
+                        className="shop col-lg-4 col-md-6 col-sm-6"
+                        key={product._id}
+                      >
+                        <div className="border-product">
+                          <Link to={`/products/${product._id}`}>
+                            <div className="shopBack">
+                              <img src={product.image} alt={product.name} />
+                            </div>
+                          </Link>
+
+                          <div className="shoptext">
+                            <p>
+                              <Link to={`/products/${product._id}`}>
+                                {product.name}
+                              </Link>
+                            </p>
+
+                            <Rating
+                              value={product.rating}
+                              text={`${product.numReviews} reviews`}
+                            />
+                            <h3>${product.price}</h3>
+                          </div>
                         </div>
-                    </div>
-                </div>
+                      </div>
+                    ))}
+                    {/* Pagination */}
+                    {/* <Pagination
+                      pages={pages}
+                      page={page}
+                      keyword={keyword ? keyword : ""}
+                    /> */}
+                    <Pagination/>
+                  </>
+                )}
+              </div>
             </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default ShopSection;
