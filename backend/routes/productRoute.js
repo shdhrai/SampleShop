6e4f2c240023a1b9a6faf96dd8ba07daa99ce311
Anujjadaun <anujjadaun97@gmail.com>
2022-07-07 21:40:29 +0530

    added responsive.css in dashboard, created app.js and corrected shopsection.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,7 +8,14 @@ const productRouter = express.Router();
 productRouter.get(
   "/",
   expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
+    const keyword=req.query.keyword?{
+      name:{
+        $regex:req.query.keyword,
+        options:"i"
+      }
+    }
+    :{}
+    const products = await Product.find({...keyword});
     res.send(products);
   })
 );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,14 +4,18 @@ import Footer from '../components/Footer';
 import ShopSection from '../components/homeComponents/ShopSection';
 import CalltoActionSection from '../components/homeComponents/CalltoActionSection';
 import ContactInfo from '../components/homeComponents/ContactInfo';
-import Categories from '../components/homeComponents/Categories'
+import Categories from '../components/homeComponents/Categories';
+import { useParams } from 'react-router';
 
 export default function HomeScreen() {
+  const params=useParams();
+  const keyword=params.keyword;
+
   return (
     <div>
       <Header/>
       <Categories/>
-      <ShopSection/>
+      <ShopSection keyword={keyword} />
       <CalltoActionSection/>
       <ContactInfo/>
       <Footer/>
