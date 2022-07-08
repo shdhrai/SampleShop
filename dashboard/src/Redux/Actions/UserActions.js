c7f19aa9de2ddda24a034663071220ce5ec74362
Anujjadaun <anujjadaun97@gmail.com>
2022-07-08 23:02:10 +0530

    getusers by admin created and added in usercomp.

--- dashboard/src/Redux/Actions/UserActions.js
+++ dashboard/src/Redux/Actions/UserActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import {
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_RESET,
+  USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -47,5 +51,41 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_LIST_RESET });
 };
 
+//All users
+export const listUser = () => async (dispatch,getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    });
+
+    const {
+      userLogin:{userInfo},
+    }=getState();
+
+    const config = {
+      headers: {
+        Authorization:`Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get('/api/users',config);
+
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
